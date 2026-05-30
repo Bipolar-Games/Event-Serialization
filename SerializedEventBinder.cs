699f19a35c5cb7b700dc3d4964b5d4e055cb2bf7
Kosmik <malpa.w.klapkach@gmail.com>
2026-05-30 23:21:15 +0200

    Add correct SerializedEventBinder behavior when components change
    Remove SerializedEventBinder if there's no ISerializedEventHost anymore
    Bind newly added hosts in runtime

--- SerializedEventBinder.cs
+++ SerializedEventBinder.cs
@@ -10,6 +10,7 @@ namespace Bipolar.EventSerialization
 
     [AddComponentMenu("")]
     [DisallowMultipleComponent]
+    [ExecuteAlways]
     public class SerializedEventBinder : MonoBehaviour
     {
         private readonly List<ISerializedEventHost> serializedEventHosts = new List<ISerializedEventHost>();
@@ -17,24 +18,92 @@ namespace Bipolar.EventSerialization
         private void Reset()
         {
             //hideFlags = HideFlags.HideInInspector;
+#if UNITY_EDITOR
+            UnityEditor.ObjectChangeEvents.changesPublished += ObjectChangeEvents_changesPublished;
+#endif
         }
 
         private void Awake()
         {
-            GetComponents(serializedEventHosts);
-            foreach (var host in serializedEventHosts)
+            if (Application.isPlaying)
             {
-                host.BindEvents();
+                GetComponents(serializedEventHosts);
+                foreach (var host in serializedEventHosts)
+                {
+                    host.BindEvents();
+                }
             }
+        }
+
+#if UNITY_EDITOR
+        private void ObjectChangeEvents_changesPublished(ref UnityEditor.ObjectChangeEventStream stream)
+        {
+            if (HasGameObjectStructureChanged(stream))
+            {
+                HandleStructureChange();
+            }
+        }
+
+        private bool HasGameObjectStructureChanged(UnityEditor.ObjectChangeEventStream stream)
+        {
+            for (int i = 0; i < stream.length; i++)
+                if (stream.GetEventType(i) == UnityEditor.ObjectChangeKind.ChangeGameObjectStructure)
+                    return true;
+
+            return false;
+        }
+
+        private void HandleStructureChange()
+        {
+            var hosts = GetComponents<ISerializedEventHost>();
+            if (hosts.Length <= 0)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+
+            if (Application.isPlaying)
+            {
+                for (int i = serializedEventHosts.Count - 1; i >= 0; i--)
+                    if (serializedEventHosts[i] == null)
+                        serializedEventHosts.RemoveAt(i);
+
+                foreach (var host in hosts)
+                {
+                    if (serializedEventHosts.IndexOf(host) < 0)
+                    {
+                        serializedEventHosts.Add(host);
+                        host.BindEvents();
+                    }
+                }
+            }
+        }
+#endif
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            UnityEditor.ObjectChangeEvents.changesPublished -= ObjectChangeEvents_changesPublished;
+            UnityEditor.ObjectChangeEvents.changesPublished += ObjectChangeEvents_changesPublished;
+#endif
+        }
+
+        private void OnDestroy()
+        {
+#if UNITY_EDITOR
+            UnityEditor.ObjectChangeEvents.changesPublished -= ObjectChangeEvents_changesPublished;
+#endif
         }
     }
 
 #if UNITY_EDITOR
-    [UnityEditor.CustomEditor(typeof(SerializedEventBinder))]
-    internal class SerializedEventBinderEditor : UnityEditor.Editor
+    namespace Editor
     {
-        public override void OnInspectorGUI() { }
+        [UnityEditor.CustomEditor(typeof(SerializedEventBinder))]
+        internal class SerializedEventBinderEditor : UnityEditor.Editor
+        {
+            public override void OnInspectorGUI() { }
+        }
     }
 #endif
-
 }
